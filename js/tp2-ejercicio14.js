86258c708942f0856ecda58fcd7b24cb893a0f3a
EmirParadi <emir.alejandro.paradi@gmail.com>
2023-11-06 03:39:23 -0300

    Ejercicio15 terminado

--- js/tp2-ejercicio14.js
+++ js/tp2-ejercicio14.js
@@ -1,7 +1,7 @@
 /*14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.*/
 
 const texto = prompt(`Ingrese su texto`);
-const textoEnArray = texto.split("");
+let textoEnArray = texto.split("");
 let recorrerArray = 0;
 
 
